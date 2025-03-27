3613f0010775e784227f37bfd58713f5a407782d
freddiegi02 <freddiegi@icloud.com>
2025-03-27 09:48:23 +0000

    Task1 CommandLine

--- Task1.py
+++ Task1.py
@@ -3,10 +3,27 @@ from processLVIS import lvisGround #Importing lvisGround class from processLVIS
 from pyproj import Proj, transform #Importing Proj and transform to change the CRS
 from matplotlib import pyplot as plt #Import for plotting
 import numpy as np #Import for numerical operations 
-from tiffExample import writeTiff #Import writetiff function from tiff example 
+import argparse
 
 
-##########################################S
+
+##########################################
+
+def getCmdArgs():
+  # function description for use within python
+  '''
+  Get commandline arguments
+  '''
+  # Create an argparse object with a description
+  ap = argparse.ArgumentParser(description=("An illustration of a command line parser"))
+  # Add a positional argument for the input filename (string)
+  ap.add_argument("filename",type=str,help=("Input filename"))
+  # Add a positional argument for the waveform index (integer)
+  ap.add_argument("waveform",type=int,help=("Input Number"))
+  # Parse the command-line arguments
+  args = ap.parse_args()
+  # return that object from this function
+  return args
 
 class plotLVIS(lvisGround):
   '''A class, ineriting from lvisData
@@ -46,9 +63,10 @@ class plotLVIS(lvisGround):
 
 if __name__=="__main__":
   '''Main block'''
-
-# File path
-filename='/geos/netdata/oosa/assignment/lvis/2009/ILVIS1B_AQ2009_1020_R1408_068453.h5'
+  
+args = getCmdArgs() #Get the command line arguments
+filename = args.filename   # Store the input filename 
+waveform = args.waveform   #Store the waveform index
 
 # create instance of class with "onlyBounds" flag
 b=plotLVIS(filename,onlyBounds=True)
@@ -56,12 +74,12 @@ b=plotLVIS(filename,onlyBounds=True)
 # Define spatial boundaries 
 x0=b.bounds[0]
 y0=b.bounds[1]
-x1=(b.bounds[2]-b.bounds[0])/10+b.bounds[0]
-y1=(b.bounds[3]-b.bounds[1])/10+b.bounds[1]
+x1=(b.bounds[2]-b.bounds[0])/1+b.bounds[0]
+y1=(b.bounds[3]-b.bounds[1])/1+b.bounds[1]
 
 # read in all data within our spatial subset
 lvis=plotLVIS(filename,minX=x0,minY=y0,maxX=x1,maxY=y1,setElev=True)
 
-#Plot the first waveform
-lvis.plotWave(0)
+#Plot the choosen waveform
+lvis.plotWave(waveform)
 
