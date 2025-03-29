ee96a58b1dd6661fd2be0b4221108d944f89048d
freddiegi02 <freddiegi@icloud.com>
2025-03-29 14:48:12 +0000

    Task 2 Memory

--- Task2.py
+++ Task2.py
@@ -10,7 +10,10 @@ from pyproj import Proj, transform #Importing Proj and transform to change the C
 from matplotlib import pyplot as plt #Import for plotting
 import numpy as np #Import for numerical operations 
 import argparse #Import for handling command-line arguments 
-
+from rasterio.merge import merge #Import for merging GeoTIFF files
+import rasterio #Import to help with Raster Data
+from glob import glob #Import to help with multiple files and folders
+import os #Import for file and directory handling
 
 
 tracemalloc.start()
@@ -51,6 +54,37 @@ class plotLVIS(lvisGround):
     writeTiff(self.zG,self.long,self.lat,res,filename=outName,epsg=3031)
     return
   
+  def mergeDEM(self):
+  
+    # Get the current working directory (PWD)
+    current_dir = os.getcwd()
+
+    # Use the current working directory for the input files and output file
+    dirpath = glob(f"{current_dir}/T2*tif")
+    out_fp = f"{current_dir}/Merged2009.tif"
+
+    mosacic_files = []
+
+    for files in dirpath:
+        src = rasterio.open(files)
+        mosacic_files.append(src)
+
+    mosaic, out_trans = merge(mosacic_files)
+
+    out_meta = src.meta.copy()
+
+    out_meta.update({
+        "driver": "GTiff",
+        "height": mosaic.shape[1],
+        "width": mosaic.shape[2],
+        "transform": out_trans,
+        "count": mosaic.shape[0],
+        "dtype": mosaic.dtype,
+    })
+
+    with rasterio.open(out_fp, "w", **out_meta) as dest:
+        dest.write(mosaic)
+
 def norm_lon(lon):
     #Normalise longitude to ensure values remain within a valid range (0-360 degrees)
     return (lon) % 360
@@ -71,8 +105,8 @@ if __name__=="__main__":
 
   file_count = 1
 
-  step_x = (x1 - x0) / 4 # Divide the x-range into 6 tiles
-  step_y = (y1 - y0) / 4  # Divide the y-range into 6 tiles
+  step_x = (x1 - x0) / 6 # Divide the x-range into 6 tiles
+  step_y = (y1 - y0) / 6  # Divide the y-range into 6 tiles
 
   for tile_x0 in np.arange(x0,x1, step_x):
     tile_x1=tile_x0+step_x
@@ -86,13 +120,23 @@ if __name__=="__main__":
 
         lvis.reprojectLVIS(3031) # Reproject the data to the Antarctic Polar Stereographic projection (EPSG 3031)
         lvis.estimateGround()  
-        outName = f"lvisDEM2009{file_count}.tif"  # Estimate ground elevation from LVIS data
+        outName = f"T2_DEM_{file_count}.tif"  # Estimate ground elevation from LVIS data
         file_count +=1 #Increase the file size by one each time
         lvis.writeDEM(res, outName) # Write the DEM data to a GeoTIFF file with the specified resolution
       
       except AttributeError as e:
         print(f"{filename} Skipped")
   
+lvis.mergeDEM()
+current, peak = tracemalloc.get_traced_memory()
 
-print("Memory peak usage:", tracemalloc.get_traced_memory())
+# Convert bytes to MB
+current_mb = current / 10**9
+peak_mb = peak / 10**9
+
+# Print memory usage details
+print(f"Current memory usage: {current_mb:.2f} GB")
+print(f"Peak memory usage: {peak_mb:.2f} GB")
+
+# Stop tracing memory
 tracemalloc.stop()
